94c64bf63513e60f245d09d5792d7cbb9c05cd49
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2023-11-13 20:06:44 +0100

    create users resource

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    const users = [
+      { id: '1', name: 'Albert' },
+      { id: '2', name: 'Juan' },
+    ];
+    return users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
